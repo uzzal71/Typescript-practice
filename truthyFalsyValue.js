6fd113c1ef2fc4e56787b92d0d72ba2bbbc871ab
uzzal71 <uzzalroy.acm@gmail.com>
2023-06-28 23:15:51 +0600

    Truthy Or falsy value details

--- truthyFalsyValue.js
+++ truthyFalsyValue.js
@@ -1,1 +1,10 @@
 // truthly falsy
+var myVar = [4, 5];
+
+// false, 0, "", null, undefined, NaN
+
+if ( myVar === true ) {
+    console.log("i am truthy");
+} else {
+    console.log("i am falsy");
+}
