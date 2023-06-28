7d7a94f3b79f97fa35cb6fb4a293f767f2ee7529
uzzal71 <uzzalroy.acm@gmail.com>
2023-06-29 00:13:56 +0600

    Array concat

---
+++ arrayConcat.js
@@ -0,0 +1,8 @@
+// Array.prototype.concat();
+var number1 = [1, 2, 3, 4, 5];
+var number2 = [6, 7, 8, 9, 10];
+var number3 = [6, 7, 8, 9, 10];
+var number4 = [6, 7, 8, 9, 10];
+
+var result = number1.concat(number2, number3, number4);
+console.log(result);
