67330fc42c51705b02b3a668dc301bb3bd4a5384
uzzal71 <uzzalroy.acm@gmail.com>
2023-06-28 11:26:34 +0600

    flat array

---
+++ arrowFunction.js
@@ -0,0 +1,10 @@
+const userInfo = () => {
+    return {
+        name: "Uzzal Roy",
+        age: 26
+    };
+}
+console.log(userInfo());
+
+const singleReturn = () => 5 * 5;
+console.log(singleReturn());
