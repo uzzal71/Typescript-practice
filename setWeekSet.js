979adab8f90e7a323d9733147eca565e703ef718
uzzal71 <uzzalroy.acm@gmail.com>
2023-06-30 01:41:22 +0600

    Set completed

--- setWeekSet.js
+++ setWeekSet.js
@@ -19,3 +19,15 @@ let mySet = new Set(myArray);
 
 log([...mySet]);
 log(Array.from(myArray));
+
+let a = new Set([1, 2, 3]);
+let b = new Set([4, 3, 2]);
+
+let union = new Set([...a, ...b]);
+log(union);
+
+let intersection = new Set([...a].filter(x => b.has(x)));
+log(intersection);
+
+let difference = new Set([...a].filter(x => !b.has(x)))
+log(difference);
