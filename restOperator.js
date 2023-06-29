cc484f4e9e601ffdd975fae1055748e46d4d8503
uzzal71 <uzzalroy.acm@gmail.com>
2023-06-29 22:57:59 +0600

    Rest Operator

---
+++ restOperator.js
@@ -0,0 +1,11 @@
+// rest operator
+function myFunc(a, ...params) {
+    // console.log(a, b);
+    // console.log(arguments);
+    console.log(a);
+    console.log(params)
+}
+
+// myFunc(3, 5);
+// myFunc(2, 3, 4, 5, 6);
+myFunc(2, 3, 4, 5, 6);
